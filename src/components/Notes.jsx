ad23aad611c0f3c9a32182e0f6de7dc21d33f1d5
Aditya Srivastava <srivastava9aditya@gmail.com>
2024-05-18 04:20:29 +0530

    notes responsive

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -10,16 +10,16 @@ const Notes = ({ notes, onAddNote, onDeleteNote, onEditNote, onTimestampClick })
   };
 
   return (
-    <div className="notes-section max-w-xl min-w-full mt-6 p-4 border border-black rounded bg-white">
+    <div className="notes-section max-w-xl min-w-full mt-6 p-4 border border-black rounded bg-white overflow-auto">
       <h2 className="text-xl font-bold mb-4">My Notes</h2>
       <p className="text-gray-500 mb-4">All your notes at a single place. Click on any note to go to specific timestamp in the video.</p>
       <ul id="notes-list" className="list-none p-0">
         {notes.map(note => (
           <li key={note.id} className="flex justify-between items-center mb-2 p-2 border-b">
-            <div className="flex flex-col">
-              <span className="cursor-pointer text-blue-500" onClick={() => onTimestampClick(note.time)}>
-                {note.timestamp}: {note.content}
-              </span>
+            <div className="flex flex-col overflow-auto mr-2">
+              <p className="cursor-pointer text-blue-500 text-wrap max-w-[80%]" onClick={() => onTimestampClick(note.time)}>
+                {note.timestamp}:<span className="break-words">{note.content}</span>
+              </p>
               <span className="text-gray-500 text-sm">{note.date}</span>
             </div>
             <div>
@@ -35,7 +35,7 @@ const Notes = ({ notes, onAddNote, onDeleteNote, onEditNote, onTimestampClick })
           value={noteContent}
           onChange={(e) => setNoteContent(e.target.value)}
           placeholder="Add a note..."
-          className="flex-1 p-2 border rounded"
+          className="flex-1 p-2 border rounded text-wrap max-w-full"
         />
         <button onClick={handleAddNote} className="ml-2 p-2 bg-blue-500 text-white rounded">Add Note</button>
       </div>
